02060ad0a5d128ef1242e7664b0e31e0ab8ebae8
GTGAMESTUDIO <97919256+GTGAMESTUDIO@users.noreply.github.com>
2025-04-24 14:13:58 +0300

    Update 6vector.cpp

--- 6vector.cpp
+++ 6vector.cpp
@@ -1,1 +1,10 @@
+#include <vector>
 
+std::vector<int> nums = {1, 2, 3, 4, 5};
+
+nums.push_back(6);  // добавляет элемент в конец
+nums.pop_back();    // удаляет последний элемент
+
+for (int num : nums) {
+    std::cout << num << " ";  // 1 2 3 4 5
+}
