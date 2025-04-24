58c2572511860b5263074a59650ca158980702bd
GTGAMESTUDIO <97919256+GTGAMESTUDIO@users.noreply.github.com>
2025-04-24 14:26:24 +0300

    Create 3. std::binary_search — бинарный поиск.cpp

---
+++ 3. std::binary_search — бинарный поиск.cpp
@@ -0,0 +1,9 @@
+Зачем: Поиск в отсортированном массиве за O(log n).
+
+
+bool found = std::binary_search(v.begin(), v.end(), 3); // true
+Связка с:
+
+std::lower_bound (первый элемент ≥ X).
+
+std::upper_bound (первый элемент > X).
